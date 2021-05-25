bffb1900426bcc88b1480fd9db2ee2e0b47a45d6
Andreas <70552989+WinniWonka@users.noreply.github.com>
2021-05-26 00:00:47 +0700

    signup/login 70%

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:cinema_premier/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'pages/home_page.dart';
+import 'pages/login_screen.dart';
+import 'pages/signup_screen.dart';
+import 'models/authentication.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+            value: Authentication(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Cinema Premier',
+        theme: ThemeData(
+          primaryColor: Colors.blue,
+        ),
+        home: LoginScreen(),
+        routes: {
+          SignupScreen.routeName: (ctx)=> SignupScreen(),
+          LoginScreen.routeName: (ctx)=> LoginScreen(),
+          HomePage.routeName: (ctx)=> HomePage(),
+        },
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
+  static const routeName = '/home';
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
